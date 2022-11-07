057bf6876eac46f99db5a4f2886f5ec8d9dd1ddb
Austerlitz-0 <1477567612@qq.com>
2022-11-07 20:39:17 +0800

    get协议传数据

--- myapp/src/cn/edu/swu/ColorPallet.java
+++ myapp/src/cn/edu/swu/ColorPallet.java
@@ -13,9 +13,9 @@ public class ColorPallet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-//        int red=Integer.valueOf(request.getParameter("red"));
-//        int green = Integer.valueOf(request.getParameter("green"));
-//        int blue = Integer.valueOf(request.getParameter("blue"));
+        int red=Integer.valueOf(request.getParameter("red"));
+        int green = Integer.valueOf(request.getParameter("green"));
+        int blue = Integer.valueOf(request.getParameter("blue"));
         int total=Integer.valueOf(request.getParameter("total"));
         int step=Integer.valueOf(request.getParameter("step"));
         String describe = request.getParameter("describe");
@@ -23,13 +23,12 @@ public class ColorPallet extends HttpServlet {
         response.setContentType("text/html");
         try(Writer writer=response.getWriter()){
             writer.write("<center><h1>This message comes from Servlet</h1></center>");
-            int red=255,green=230,blue=230;
             for(int i=0;i<total;i++)
             {
                 green-=step*i;
                 blue-=step*i;
                 writer.write(
-                   String.format("<div width='200px' height='20px' style='background-color:rgb(%s %s %s)' >&nbsp;</div><br>"
+                   String.format("<center><div style='width:200px;height:20px;background-color:rgb(%s %s %s)' ></div><center>"
                          ,red,green,blue)
                 );
 
