ace77a412e398fa9f5d8d64818b9c7a1d9ce3c0c
Austerlitz-0 <1477567612@qq.com>
2022-11-07 21:14:57 +0800

    Post方法

--- myapp/src/cn/edu/swu/ColorPallet.java
+++ myapp/src/cn/edu/swu/ColorPallet.java
@@ -12,6 +12,11 @@ public class ColorPallet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doPost(request,response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         int red=Integer.valueOf(request.getParameter("red"));
         int green = Integer.valueOf(request.getParameter("green"));
@@ -22,17 +27,20 @@ public class ColorPallet extends HttpServlet {
 
         response.setContentType("text/html");
         try(Writer writer=response.getWriter()){
-            writer.write("<center><h1>This message comes from Servlet</h1></center>");
+            writer.write("<center>"+describe+"</h1></center>");
             for(int i=0;i<total;i++)
             {
                 green-=step*i;
                 blue-=step*i;
                 writer.write(
-                   String.format("<center><div style='width:200px;height:20px;background-color:rgb(%s %s %s)' ></div><center>"
-                         ,red,green,blue)
+                        String.format("<center><div style='width:200px;height:20px;background-color:rgb(%s %s %s)' ></div><center>"
+                                ,red,green,blue)
                 );
 
             }
         }
     }
+
+
+
 }
