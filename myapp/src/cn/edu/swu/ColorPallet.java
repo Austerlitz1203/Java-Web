ac48c282a0d853807ef489113c3ae516237a1619
Austerlitz-0 <1477567612@qq.com>
2022-11-07 18:36:07 +0800

    一些修改

--- myapp/src/cn/edu/swu/ColorPallet.java
+++ myapp/src/cn/edu/swu/ColorPallet.java
@@ -1,30 +1,38 @@
 package cn.edu.swu;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.Writer;
-
+@WebServlet("/color")
 public class ColorPallet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+//        int red=Integer.valueOf(request.getParameter("red"));
+//        int green = Integer.valueOf(request.getParameter("green"));
+//        int blue = Integer.valueOf(request.getParameter("blue"));
         int total=Integer.valueOf(request.getParameter("total"));
         int step=Integer.valueOf(request.getParameter("step"));
+        String describe = request.getParameter("describe");
 
         response.setContentType("text/html");
         try(Writer writer=response.getWriter()){
             writer.write("<center><h1>This message comes from Servlet</h1></center>");
-
-            int red=266,green=230,blue=230;
+            int red=255,green=230,blue=230;
             for(int i=0;i<total;i++)
             {
                 green-=step*i;
                 blue-=step*i;
-                writer.write("<div width='200px' height='20px'></div>");
+                writer.write(
+                   String.format("<div width='200px' height='20px' style='background-color:rgb(%s %s %s)' >&nbsp;</div><br>"
+                         ,red,green,blue)
+                );
+
             }
         }
     }
